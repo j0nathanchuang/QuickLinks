613d52bcf644d40cddd68419b56b5a182cbdc768
Jonathan Chuang <jochuang@umich.edu>
2020-05-09 19:49:29 -0400

    initial commit

---
+++ popup.js
@@ -0,0 +1,48 @@
+const input0 = document.getElementById("link0");
+const button0 = document.getElementById("btn0");
+button0.onclick = function() {
+  /*Select the url*/
+  input0.select();
+  /*Copy text*/
+  document.execCommand("Copy");
+}
+const input = document.getElementById("link");
+const button = document.getElementById("btn");
+button.onclick = function() {
+  /*Select the url*/
+  input.select();
+  /*Copy text*/
+  document.execCommand("Copy");
+}
+const input1 = document.getElementById("link1");
+const button1 = document.getElementById("btn1");
+button1.onclick = function() {
+  /*Select the url*/
+  input1.select();
+  /*Copy text*/
+  document.execCommand("Copy");
+}
+const input2 = document.getElementById("link2");
+const button2 = document.getElementById("btn2");
+button2.onclick = function() {
+  /*Select the url*/
+  input2.select();
+  /*Copy text*/
+  document.execCommand("Copy");
+}
+const input3 = document.getElementById("link3");
+const button3 = document.getElementById("btn3");
+button3.onclick = function() {
+  /*Select the url*/
+  input3.select();
+  /*Copy text*/
+  document.execCommand("Copy");
+}
+const input4 = document.getElementById("link4");
+const button4 = document.getElementById("btn4");
+button4.onclick = function() {
+  /*Select the url*/
+  input4.select();
+  /*Copy text*/
+  document.execCommand("Copy");
+}
---
+++ popup.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+LinkedIn: <?php echo $_POST["linkedin"]; ?><br>
+GitHub: <?php echo $_POST["github"]; ?><br>
+Facebook: <?php echo $_POST["facebook"]; ?><br>
+E-mail: <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
